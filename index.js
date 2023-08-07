d03daaeb3c6978408e2d3deca2bb7ed7b5cfe833
Applebees1 <104962202+Applebees1@users.noreply.github.com>
2023-08-06 22:44:53 -0700

    finish lab

---
+++ index.js
@@ -0,0 +1,70 @@
+// Question #1 //
+let numberOfStudents = 17;
+
+if (numberOfStudents > 20){console.log("classroom too small");
+} else { console.log ("classroom size acceptable");
+}; 
+
+//Question #2 //
+
+let numberOfHouses = 5; 
+
+if (numberOfHouses > 2){console.log("you need to sell more houses");
+} 
+if (numberOfHouses <= 2){
+  console.log("seems reasonable");
+}
+
+//Question #3//
+
+let bill = 20;
+
+if (bill > 15) {
+  console.log("we should split the bill");
+} if (bill <= 15) {console.log("I'll get the bill this time");
+}
+
+//Question 4//
+
+let studentsScore1 = 55; 
+let studentsScore2 = 60; 
+let studentsScore3 = 70; 
+let studentsScore4 = 80; 
+let studentsScore5 = 90; 
+
+if (studentsScore1 <=55){console.log ("Not to good")}
+if (studentsScore2 <=60){console.log ("ehhhh")}
+if (studentsScore3 <=70){console.log ("Keep trying")}
+if (studentsScore4 <=80){console.log ("nice!")}
+if (studentsScore5 <=90){console.log ("Doing good!")}
+
+else {console.log("ove 90! great!")}
+
+// Question 5//
+//Example 1 //
+let myName = "Mike";
+
+if (myName === "Jessie"){
+  console.log("Correct!")
+ } else{ console.log("Incorrect!");
+}
+
+//Example 2//
+let personAge = 85;
+
+if (personAge > 60 && personAge <= 80) {
+  console.log("Not Too Old!");
+} else if (personAge > 30 && personAge <= 60) {
+  console.log("Still Young!");
+} else {
+  console.log("Good Age!");
+}
+
+//Example 3//
+let checkNumber = 123456; 
+
+if (checkNumber === 145369){
+  console.log("True")
+} else{
+  console.log("False")
+}
